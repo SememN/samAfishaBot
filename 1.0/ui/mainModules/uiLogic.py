b58441868bf7da7f1311dc1c2c5cea70b03371a6
Sam <51040158+SememN@users.noreply.github.com>
2020-04-14 15:34:52 +0500

    Add files via upload

---
+++ 1.0/ui/mainModules/uiLogic.py
@@ -0,0 +1,30 @@
+#for the app
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+import appSetup
+#mainWindow
+from appSetup import MainWindow, ui, app
+#noAreaOrCatException
+from appSetup import Dialog
+#module "food" need it
+import requests
+from bs4 import BeautifulSoup
+import openpyxl
+import food
+
+#start main window
+appSetup.MainWindow.show()
+
+#hook logic
+def except1():
+    if ui.comboBox.currentText() == "РАЙОН":
+        appSetup.Dialog.show()
+    if ui.comboBox_2.currentText() == "ВИД ЕДЫ":
+        appSetup.Dialog.show()
+    else:
+        food.showFood()
+#app logic
+ui.pushButton.clicked.connect(except1)
+
+#Run main loop 
+sys.exit(app.exec_())
