9310796b183fb898a5c6a7a918a0ae0c95e54c17
Sam <51040158+SememN@users.noreply.github.com>
2020-05-29 18:08:00 +0500

    Update appSetup.py

--- 1.0/ui/mainModules/appSetup.py
+++ 1.0/ui/mainModules/appSetup.py
@@ -1,19 +1,19 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from appUi import appUi
 from appUi import noAreaOrCatExceptionUi
+from appUi import foodFind
 import sys 
 
 #MAIN WINDOW
 #create app
 app = QtWidgets.QApplication(sys.argv)
-#create form and init ui
 MainWindow = QtWidgets.QMainWindow()
-application = appUi.Ui_MainWindow
-ui = application()
-ui.setupUi(MainWindow)
-#set the size of the main window
+mainWin = appUi.Ui_MainWindow
+mainUi = mainWin()
+mainUi.setupUi(MainWindow)
+
 MainWindow.setFixedSize(1025, 620) 
-#main windo title
+
 MainWindow.setWindowTitle("SamAfisha")
 
 #noAreaOrCatException
@@ -21,3 +21,10 @@ Dialog = QtWidgets.QDialog()
 except1 = noAreaOrCatExceptionUi.Ui_Dialog
 uiExcept1 = except1()
 uiExcept1.setupUi(Dialog)    
+
+#FOODFIND WINDOW
+foodFindWin = QtWidgets.QApplication(sys.argv)
+foodWin = QtWidgets.QWidget()
+foodWindow = foodFind.Ui_FoodWin
+foodWindowUi = foodWindow()
+foodWindowUi.setupUi(foodWin)
