074ea3f7222f6ad6af63ade9ebbf9af163ce5772
Sam <51040158+SememN@users.noreply.github.com>
2020-05-29 18:09:39 +0500

    Add files via upload

---
+++ 1.0/ui/mainModules/appUi/foodFind.py
@@ -0,0 +1,323 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'G:\Pytonchik\In progress\samAfisha\1.0\ui\mainModules\appUi\foodFind.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_FoodWin(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(876, 984)
+        Form.setStyleSheet("color: rgb(195, 109, 25);\n"
+"")
+        self.graphicsView = QtWidgets.QGraphicsView(Form)
+        self.graphicsView.setGeometry(QtCore.QRect(0, 0, 891, 991))
+        self.graphicsView.setStyleSheet("background-color: rgb(40,40,40);\n"
+"color: 195,109,25\n"
+"")
+        self.graphicsView.setObjectName("graphicsView")
+        self.Name = QtWidgets.QLabel(Form)
+        self.Name.setGeometry(QtCore.QRect(20, 10, 331, 51))
+        self.Name.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name.setObjectName("Name")
+        self.check_online = QtWidgets.QLabel(Form)
+        self.check_online.setGeometry(QtCore.QRect(700, 10, 161, 51))
+        self.check_online.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online.setObjectName("check_online")
+        self.adress = QtWidgets.QLabel(Form)
+        self.adress.setGeometry(QtCore.QRect(350, 10, 351, 51))
+        self.adress.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress.setObjectName("adress")
+        self.Name_2 = QtWidgets.QLabel(Form)
+        self.Name_2.setGeometry(QtCore.QRect(20, 70, 331, 51))
+        self.Name_2.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name_2.setObjectName("Name_2")
+        self.Name_3 = QtWidgets.QLabel(Form)
+        self.Name_3.setGeometry(QtCore.QRect(20, 130, 331, 51))
+        self.Name_3.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name_3.setObjectName("Name_3")
+        self.Name_4 = QtWidgets.QLabel(Form)
+        self.Name_4.setGeometry(QtCore.QRect(20, 190, 331, 51))
+        self.Name_4.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name_4.setObjectName("Name_4")
+        self.Name_5 = QtWidgets.QLabel(Form)
+        self.Name_5.setGeometry(QtCore.QRect(20, 250, 331, 51))
+        self.Name_5.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name_5.setObjectName("Name_5")
+        self.Name_6 = QtWidgets.QLabel(Form)
+        self.Name_6.setGeometry(QtCore.QRect(20, 310, 331, 51))
+        self.Name_6.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name_6.setObjectName("Name_6")
+        self.Name_7 = QtWidgets.QLabel(Form)
+        self.Name_7.setGeometry(QtCore.QRect(20, 370, 331, 51))
+        self.Name_7.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name_7.setObjectName("Name_7")
+        self.Name_8 = QtWidgets.QLabel(Form)
+        self.Name_8.setGeometry(QtCore.QRect(20, 430, 331, 51))
+        self.Name_8.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name_8.setObjectName("Name_8")
+        self.Name_9 = QtWidgets.QLabel(Form)
+        self.Name_9.setGeometry(QtCore.QRect(20, 490, 331, 51))
+        self.Name_9.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name_9.setObjectName("Name_9")
+        self.Name_10 = QtWidgets.QLabel(Form)
+        self.Name_10.setGeometry(QtCore.QRect(20, 550, 331, 51))
+        self.Name_10.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name_10.setObjectName("Name_10")
+        self.Name_11 = QtWidgets.QLabel(Form)
+        self.Name_11.setGeometry(QtCore.QRect(20, 610, 331, 51))
+        self.Name_11.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name_11.setObjectName("Name_11")
+        self.Name_12 = QtWidgets.QLabel(Form)
+        self.Name_12.setGeometry(QtCore.QRect(20, 670, 331, 51))
+        self.Name_12.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name_12.setObjectName("Name_12")
+        self.Name_13 = QtWidgets.QLabel(Form)
+        self.Name_13.setGeometry(QtCore.QRect(20, 730, 331, 51))
+        self.Name_13.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name_13.setObjectName("Name_13")
+        self.Name_14 = QtWidgets.QLabel(Form)
+        self.Name_14.setGeometry(QtCore.QRect(20, 790, 331, 51))
+        self.Name_14.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name_14.setObjectName("Name_14")
+        self.Name_15 = QtWidgets.QLabel(Form)
+        self.Name_15.setGeometry(QtCore.QRect(20, 850, 331, 51))
+        self.Name_15.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.Name_15.setObjectName("Name_15")
+        self.adress_2 = QtWidgets.QLabel(Form)
+        self.adress_2.setGeometry(QtCore.QRect(350, 70, 351, 51))
+        self.adress_2.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress_2.setObjectName("adress_2")
+        self.adress_3 = QtWidgets.QLabel(Form)
+        self.adress_3.setGeometry(QtCore.QRect(350, 130, 341, 51))
+        self.adress_3.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress_3.setObjectName("adress_3")
+        self.adress_4 = QtWidgets.QLabel(Form)
+        self.adress_4.setGeometry(QtCore.QRect(350, 190, 351, 51))
+        self.adress_4.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress_4.setObjectName("adress_4")
+        self.adress_5 = QtWidgets.QLabel(Form)
+        self.adress_5.setGeometry(QtCore.QRect(350, 250, 341, 51))
+        self.adress_5.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress_5.setObjectName("adress_5")
+        self.adress_6 = QtWidgets.QLabel(Form)
+        self.adress_6.setGeometry(QtCore.QRect(350, 310, 341, 51))
+        self.adress_6.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress_6.setObjectName("adress_6")
+        self.adress_7 = QtWidgets.QLabel(Form)
+        self.adress_7.setGeometry(QtCore.QRect(350, 370, 341, 51))
+        self.adress_7.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress_7.setObjectName("adress_7")
+        self.adress_8 = QtWidgets.QLabel(Form)
+        self.adress_8.setGeometry(QtCore.QRect(350, 430, 341, 51))
+        self.adress_8.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress_8.setObjectName("adress_8")
+        self.adress_9 = QtWidgets.QLabel(Form)
+        self.adress_9.setGeometry(QtCore.QRect(350, 490, 341, 51))
+        self.adress_9.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress_9.setObjectName("adress_9")
+        self.adress_10 = QtWidgets.QLabel(Form)
+        self.adress_10.setGeometry(QtCore.QRect(350, 550, 341, 51))
+        self.adress_10.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress_10.setObjectName("adress_10")
+        self.adress_11 = QtWidgets.QLabel(Form)
+        self.adress_11.setGeometry(QtCore.QRect(350, 610, 331, 51))
+        self.adress_11.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress_11.setObjectName("adress_11")
+        self.adress_12 = QtWidgets.QLabel(Form)
+        self.adress_12.setGeometry(QtCore.QRect(350, 670, 341, 51))
+        self.adress_12.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress_12.setObjectName("adress_12")
+        self.adress_13 = QtWidgets.QLabel(Form)
+        self.adress_13.setGeometry(QtCore.QRect(350, 730, 341, 51))
+        self.adress_13.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress_13.setObjectName("adress_13")
+        self.adress_14 = QtWidgets.QLabel(Form)
+        self.adress_14.setGeometry(QtCore.QRect(350, 790, 341, 51))
+        self.adress_14.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress_14.setObjectName("adress_14")
+        self.adress_15 = QtWidgets.QLabel(Form)
+        self.adress_15.setGeometry(QtCore.QRect(350, 850, 341, 51))
+        self.adress_15.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;")
+        self.adress_15.setObjectName("adress_15")
+        self.check_online_2 = QtWidgets.QLabel(Form)
+        self.check_online_2.setGeometry(QtCore.QRect(700, 70, 161, 51))
+        self.check_online_2.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online_2.setObjectName("check_online_2")
+        self.check_online_3 = QtWidgets.QLabel(Form)
+        self.check_online_3.setGeometry(QtCore.QRect(700, 130, 161, 51))
+        self.check_online_3.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online_3.setObjectName("check_online_3")
+        self.check_online_4 = QtWidgets.QLabel(Form)
+        self.check_online_4.setGeometry(QtCore.QRect(700, 190, 151, 51))
+        self.check_online_4.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online_4.setObjectName("check_online_4")
+        self.check_online_5 = QtWidgets.QLabel(Form)
+        self.check_online_5.setGeometry(QtCore.QRect(700, 250, 161, 51))
+        self.check_online_5.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online_5.setObjectName("check_online_5")
+        self.check_online_6 = QtWidgets.QLabel(Form)
+        self.check_online_6.setGeometry(QtCore.QRect(700, 310, 161, 51))
+        self.check_online_6.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online_6.setObjectName("check_online_6")
+        self.check_online_7 = QtWidgets.QLabel(Form)
+        self.check_online_7.setGeometry(QtCore.QRect(700, 370, 161, 51))
+        self.check_online_7.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online_7.setObjectName("check_online_7")
+        self.check_online_8 = QtWidgets.QLabel(Form)
+        self.check_online_8.setGeometry(QtCore.QRect(700, 430, 161, 51))
+        self.check_online_8.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online_8.setObjectName("check_online_8")
+        self.check_online_9 = QtWidgets.QLabel(Form)
+        self.check_online_9.setGeometry(QtCore.QRect(700, 490, 161, 51))
+        self.check_online_9.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online_9.setObjectName("check_online_9")
+        self.check_online_10 = QtWidgets.QLabel(Form)
+        self.check_online_10.setGeometry(QtCore.QRect(700, 550, 161, 51))
+        self.check_online_10.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online_10.setObjectName("check_online_10")
+        self.check_online_11 = QtWidgets.QLabel(Form)
+        self.check_online_11.setGeometry(QtCore.QRect(700, 610, 161, 51))
+        self.check_online_11.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online_11.setObjectName("check_online_11")
+        self.check_online_12 = QtWidgets.QLabel(Form)
+        self.check_online_12.setGeometry(QtCore.QRect(700, 670, 161, 51))
+        self.check_online_12.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online_12.setObjectName("check_online_12")
+        self.check_online_13 = QtWidgets.QLabel(Form)
+        self.check_online_13.setGeometry(QtCore.QRect(700, 730, 161, 51))
+        self.check_online_13.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online_13.setObjectName("check_online_13")
+        self.check_online_14 = QtWidgets.QLabel(Form)
+        self.check_online_14.setGeometry(QtCore.QRect(700, 790, 161, 51))
+        self.check_online_14.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online_14.setObjectName("check_online_14")
+        self.check_online_15 = QtWidgets.QLabel(Form)
+        self.check_online_15.setGeometry(QtCore.QRect(700, 850, 161, 51))
+        self.check_online_15.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"")
+        self.check_online_15.setObjectName("check_online_15")
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(230, 920, 341, 51))
+        self.pushButton.setStyleSheet("font: 75 18pt \"Teko\";\n"
+"text-align: center;\n"
+"background-color: rgb(40,40,40);")
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+    
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.Name.setText(_translate("Form", "название"))
+        self.check_online.setText(_translate("Form", "открыто/закрыто"))
+        self.adress.setText(_translate("Form", "адрес"))
+        self.Name_2.setText(_translate("Form", "название"))
+        self.Name_3.setText(_translate("Form", "название"))
+        self.Name_4.setText(_translate("Form", "название"))
+        self.Name_5.setText(_translate("Form", "название"))
+        self.Name_6.setText(_translate("Form", "название"))
+        self.Name_7.setText(_translate("Form", "название"))
+        self.Name_8.setText(_translate("Form", "название"))
+        self.Name_9.setText(_translate("Form", "название"))
+        self.Name_10.setText(_translate("Form", "название"))
+        self.Name_11.setText(_translate("Form", "название"))
+        self.Name_12.setText(_translate("Form", "название"))
+        self.Name_13.setText(_translate("Form", "название"))
+        self.Name_14.setText(_translate("Form", "название"))
+        self.Name_15.setText(_translate("Form", "название"))
+        self.adress_2.setText(_translate("Form", "адрес"))
+        self.adress_3.setText(_translate("Form", "адрес"))
+        self.adress_4.setText(_translate("Form", "адрес"))
+        self.adress_5.setText(_translate("Form", "адрес"))
+        self.adress_6.setText(_translate("Form", "адрес"))
+        self.adress_7.setText(_translate("Form", "адрес"))
+        self.adress_8.setText(_translate("Form", "адрес"))
+        self.adress_9.setText(_translate("Form", "адрес"))
+        self.adress_10.setText(_translate("Form", "адрес"))
+        self.adress_11.setText(_translate("Form", "адрес"))
+        self.adress_12.setText(_translate("Form", "адрес"))
+        self.adress_13.setText(_translate("Form", "адрес"))
+        self.adress_14.setText(_translate("Form", "адрес"))
+        self.adress_15.setText(_translate("Form", "адрес"))
+        self.check_online_2.setText(_translate("Form", "открыто/закрыто"))
+        self.check_online_3.setText(_translate("Form", "открыто/закрыто"))
+        self.check_online_4.setText(_translate("Form", "открыто/закрыто"))
+        self.check_online_5.setText(_translate("Form", "открыто/закрыто"))
+        self.check_online_6.setText(_translate("Form", "открыто/закрыто"))
+        self.check_online_7.setText(_translate("Form", "открыто/закрыто"))
+        self.check_online_8.setText(_translate("Form", "открыто/закрыто"))
+        self.check_online_9.setText(_translate("Form", "открыто/закрыто"))
+        self.check_online_10.setText(_translate("Form", "открыто/закрыто"))
+        self.check_online_11.setText(_translate("Form", "открыто/закрыто"))
+        self.check_online_12.setText(_translate("Form", "открыто/закрыто"))
+        self.check_online_13.setText(_translate("Form", "открыто/закрыто"))
+        self.check_online_14.setText(_translate("Form", "открыто/закрыто"))
+        self.check_online_15.setText(_translate("Form", "открыто/закрыто"))
+        self.pushButton.setText(_translate("Form", "показать больше"))
